f6b35042cac74cdf0da43e2db8d8fe7ed2800282
Raj Maurya <itsraj63062@gmail.com>
2025-11-04 08:04:13 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-9618124962-5e731",
+  "appId": "1:834335072077:web:5d6e230b4971edc39c2104",
+  "apiKey": "AIzaSyBRb_6SP8AwSm-wsbN24IGxHg-EjdLZUOM",
+  "authDomain": "studio-9618124962-5e731.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "834335072077"
 };
